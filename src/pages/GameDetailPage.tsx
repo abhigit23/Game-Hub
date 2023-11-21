7f24fe2ba5bce477d2cdd101c55a1bd5ea0467cf
abhigit23 <abhijeetbhandari84@gmail.com>
2023-11-21 22:37:19 +0530

    Built game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import useGame from "../hooks/useGame";
+import GameTrailer from "../components/GameTrailer";
 
 function GameDetailPage() {
   const { slug } = useParams();
@@ -14,6 +15,7 @@ function GameDetailPage() {
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 }
